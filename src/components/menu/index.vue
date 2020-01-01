3b6771cd47d2bbec738f4601b72c59838afbe69e
czz <czzonet@163.com>
2020-01-01 16:58:25 +0800

    A menu

---
+++ src/components/menu/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-menu router background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+      <el-menu-item index="/main">首页</el-menu-item>
+
+      <el-submenu index="1">
+        <template slot="title">演示散点图</template>
+
+        <el-menu-item index="/main/scatter2d">2d 散点图</el-menu-item>
+        <el-menu-item index="/main/scatter3d">3d 散点图</el-menu-item>
+        <el-menu-item index="/main/scatter3d2">3d 散点图2</el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuBar"
+};
+</script>
+
+<style>
+</style>
