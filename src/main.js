5fae6814f50cb0a54fbb55e1043060a310976c40
czz <czzonet@163.com>
2020-01-01 16:57:39 +0800

    A elementui

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* 注册路由标签 */
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
 
 /* 导入配置的路由对象 */
-import router from './router'
+import router from "./router";
+
+/** element ui */
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
