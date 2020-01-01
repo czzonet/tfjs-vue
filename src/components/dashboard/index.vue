06d4255373f7972f9cd27b0c28e86096d7916766
czz <czzonet@163.com>
2020-01-01 16:57:57 +0800

    A dashboard

---
+++ src/components/dashboard/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="class-center">
+    <div>
+      <h1>使用tfjs库在浏览器机器学习并通过echart图表演示</h1>
+      <h3>机器学习库：tfjs</h3>
+      <h3>图表：echart</h3>
+      <h3>界面：elementUI</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BarDashboard"
+};
+</script>
+
+<style lang="scss" scoped>
+.class-center {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
