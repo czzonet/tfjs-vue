546f7adef5e6798be5959a701f78635cada9b331
czz <czzonet@163.com>
2020-01-01 16:58:58 +0800

    A new router path

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,11 +1,26 @@
-import Scatter2d from '../components/Scatter2d.vue'
-import Scatter3d from '../components/Scatter3d.vue'
-import Scatter3d2 from '../components/Scatter3d2.vue'
+import Scatter2d from "../components/Scatter2d.vue";
+import Scatter3d from "../components/Scatter3d.vue";
+import Scatter3d2 from "../components/Scatter3d2.vue";
+import BarLayout from "@/components/layout/index";
+import BarDashboard from "@/components/dashboard/index";
 
 const routes = [
-  { path: '/scatter2d', component: Scatter2d },
-  { path: '/scatter3d', component: Scatter3d },
-  { path: '/scatter3d2', component: Scatter3d2 },
-]
+  {
+    path: "/",
+    // component: BarLayout,
+    redirect: "/main"
+  },
+  {
+    path: "/main",
+    component: BarLayout,
+    redirect: "/main/dashboard",
+    children: [
+      { path: "dashboard", component: BarDashboard },
+      { path: "scatter2d", component: Scatter2d },
+      { path: "scatter3d", component: Scatter3d },
+      { path: "scatter3d2", component: Scatter3d2 }
+    ]
+  }
+];
 
-export default routes
+export default routes;
