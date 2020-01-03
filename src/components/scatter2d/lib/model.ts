2da2b752389c86be81a0243ad715db60a8f33cf6
czz <czzonet@163.com>
2020-01-03 19:29:21 +0800

    A calback and update the chart and message...

--- src/components/scatter2d/lib/model.ts
+++ src/components/scatter2d/lib/model.ts
@@ -1,5 +1,6 @@
 import * as tf from "@tensorflow/tfjs";
 import * as tfvis from "@tensorflow/tfjs-vis";
+import { Message } from 'element-ui'
 
 export const createModel = () => {
   let model = tf.sequential();
@@ -20,32 +21,42 @@ export const createModel = () => {
 /**
  * 准备数据 规范化
  */
-export const convertToTensor = (data = []) => {
+export const convertToTensor = (data: number[][]) => {
   const t = tf.tidy(() => {
     tf.util.shuffle(data);
 
+    /** 数组对里提取分离x，y */
     const inputs = data.map(d => d[0])
     const labels = data.map(d => d[1])
 
+    /** 形状要符合长度 数组两层嵌套二维所以2d */
     const inputTensor = tf.tensor2d(inputs, [inputs.length, 1])
     const labelTensor = tf.tensor2d(labels, [labels.length, 1])
 
-    const inputMax = inputTensor.max()
-    const inputMin = inputTensor.min()
-    const labelMax = labelTensor.max()
-    const labelMin = labelTensor.min()
+    /**
+     *  归一化
+     */
+    const normalize = (s: tf.Tensor2D) => {
+      /** 最大差 */
+      const distance = s.max().sub(s.min())
+      /** 除数为零检查 */
+      let zeroCheck = distance.dataSync()[0] == 0
+      /** 为0不进行归一化 */
+      const t = zeroCheck ? s : s.sub(s.min()).div(distance)
 
-    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin))
-    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin))
-
-    return {
-      inputs: normalizedInputs,
-      labels: normalizedLabels,
-      inputMax,
-      inputMin,
-      labelMax,
-      labelMin
+      return t
     }
+
+    let t = {} as any;
+
+    t.inputMax = inputTensor.max()
+    t.inputMin = inputTensor.min()
+    t.labelMax = labelTensor.max()
+    t.labelMin = labelTensor.min()
+    t.inputs = normalize(inputTensor)
+    t.labels = normalize(labelTensor)
+
+    return t
   })
 
   return t
@@ -54,7 +65,7 @@ export const convertToTensor = (data = []) => {
 /**
  * 训练模型
  */
-export const trainModel = async (model: any, inputs: any, labels: any) => {
+export const trainModel = async (model: any, inputs: any, labels: any, mychart: any) => {
   model.compile({
     optimizer: tf.train.adam(),
     loss: tf.losses.meanSquaredError,
@@ -64,15 +75,62 @@ export const trainModel = async (model: any, inputs: any, labels: any) => {
   const batchSize = 28
   const epochs = 50
 
+
+  let lossData = [] as any[]
+  let mseData = [] as any[]
+
+
+
   let t = await model.fit(inputs, labels, {
     batchSize,
     epochs,
     shuffle: true,
-    callbacks: tfvis.show.fitCallbacks(
-      { name: 'Training Performance' },
-      ['loss', 'mse'],
-      { height: 200, callbacks: ['onEpochEnd'] }
-    )
+    // callbacks: tfvis.show.fitCallbacks(
+    //   { name: 'Training Performance' },
+    //   ['loss', 'mse'],
+    //   { height: 200, callbacks: ['onEpochEnd'] }
+    // ),
+    callbacks: {
+      onTrainBegin: (logs: any) => {
+        console.log('onTrainBegin: ', logs);
+        Message.info('Train start!')
+
+      },
+      onTrainEnd: (logs: any) => {
+        console.log('onTrainEnd: ', logs);
+        Message.info('Train end')
+      },
+      onEpochEnd: (epoch: any, logs: any) => {
+        // console.log('onEpochBegin: ', epoch, logs);
+        lossData.push([epoch, logs.loss])
+        mseData.push([epoch, logs.mse])
+
+        mychart.setOption({
+          title: {
+            text: "Training Performance"
+          },
+          legend: {
+            data: ["loss", "mse"]
+          },
+          xAxis: [{ name: "epotch" }],
+          yAxis: [{ name: "value" }],
+          series: [
+            {
+              name: "loss",
+              type: "line",
+              smooth: true,
+              data: lossData
+            },
+            {
+              name: "mse",
+              type: "line",
+              smooth: true,
+              data: mseData
+            }
+          ]
+        });
+      }
+    }
   })
 
   return t
